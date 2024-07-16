0784c526f3ee5423a65d8c73ec4ea473e6f0eef1
Andrew King <awking46@gmail.com>
2024-07-15 22:09:26 -0400

    Adjust validation for RecordedLocations

--- app/models/recorded_location.rb
+++ app/models/recorded_location.rb
@@ -4,7 +4,7 @@ class RecordedLocation < ApplicationRecord
   validates :latitude, :longitude, presence: true
   validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
-  validates :heading, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 360 }, allow_nil: true
-  validates :adjusted_latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_nil: true
-  validates :adjusted_longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_nil: true
+  validates :heading, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 360 }, allow_blank: true
+  validates :adjusted_latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_blank: true
+  validates :adjusted_longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_blank: true
 end
