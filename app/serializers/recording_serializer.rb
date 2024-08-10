9c84a8c2b598f73145bf79826a04345430afc83b
Andrew King <awking46@gmail.com>
2024-08-10 10:10:47 -0400

    Update Recording serializer

--- app/serializers/recording_serializer.rb
+++ app/serializers/recording_serializer.rb
@@ -1,6 +1,10 @@
 class RecordingSerializer < ActiveModel::Serializer
-  attributes :id, :name, :started_at, :ended_at, :time_zone, :is_race, :start_latitude, :start_longitude
+  attributes :id, :name, :started_at, :ended_at, :time_zone, :is_race, :start_latitude, :start_longitude, :distance
   belongs_to :boat
   belongs_to :user
   belongs_to :race, optional: true
+
+  def boat
+    BoatSerializer.new(object.boat, root: false)
+  end
 end
