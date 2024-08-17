e4ff669e12b042aeab42d13cc53395bdadfbd9a0
Andrew King <awking46@gmail.com>
2024-08-17 12:16:55 -0400

    Extend validity of access tokens

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -7,7 +7,7 @@ class AccessToken < ApplicationRecord
   def generate_tokens
     self.token = SecureRandom.hex(32)
     self.refresh_token = SecureRandom.hex(32)
-    self.expires_at = 2.hours.from_now
+    self.expires_at = 12.hours.from_now
     self.refresh_token_expires_at = 30.days.from_now
   end
 end
