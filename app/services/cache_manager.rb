0c9dff966c20c458b6b96d1da62e715c486d83b6
Andrew King <awking46@gmail.com>
2024-08-19 00:14:52 -0400

    Fix cache manager methods

--- app/services/cache_manager.rb
+++ app/services/cache_manager.rb
@@ -1,19 +1,17 @@
 class CacheManager
   CACHE_VERSION = 1
 
-  class << self
-    def self.fetch(key, expires_in: 1.week, &block)
-      result = SolidCache.fetch("#{CACHE_VERSION}/#{key}", expires_in: expires_in, &block)
-      Rails.logger.info "Cache #{result.nil? ? 'MISS' : 'HIT'} for key: #{key}"
-      result
-    end
+  def self.fetch(key, expires_in: 1.week, &block)
+    result = Rails.cache.fetch("#{CACHE_VERSION}/#{key}", expires_in: expires_in, &block)
+    Rails.logger.info "Cache #{result.nil? ? 'MISS' : 'HIT'} for key: #{key}"
+    result
+  end
 
-    def delete(key)
-      SolidCache.delete("#{CACHE_VERSION}/#{key}")
-    end
+  def self.delete(key)
+    Rails.cache.delete("#{CACHE_VERSION}/#{key}")
+  end
 
-    def clear_all
-      SolidCache.clear
-    end
+  def self.clear_all
+    Rails.cache.clear
   end
 end
